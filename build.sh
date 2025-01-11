f22b5a41fa0b474b8dc8b1ac45fe2f1a64ec5709
Arturo Rodriguez <arturo.rdz3000@gmail.com>
2025-01-11 20:34:24 +0000

    build: Ignoring CMake files and making sure conan runs before build

--- build.sh
+++ build.sh
@@ -1,2 +1,19 @@
+#!/bin/bash
+
+BUILD_DIR="build"
+BUILD_CONAN_DIR="$BUILD_DIR/conan"
+
+echo "Making sure build directory exists..."
+if [ ! -d "$BUILD_DIR"]; then
+    echo "Build directory does not exist. Creating it now..."
+    mkdir -p "$BUILD_DIR"
+    echo "Build directory created!"
+fi
+
+echo "Installing dependencies..."
+conan install . --build=missing --output-folder=$BUILD_CONAN_DIR
+
+echo "Building project..."
 cmake -DCMAKE_TOOLCHAIN_FILE=build/conan/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=release
+cmake -S . -B ./build 
 cmake --build ./build
